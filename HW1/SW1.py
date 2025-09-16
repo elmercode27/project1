218ce7f7cc27354b3145ebc4ba94ba746e27a72d
elmer monje <elmermonje27@gmail.com>
2025-09-16 21:32:02 +0800

    SW1.py

--- HW1/SW1.py
+++ HW1/SW1.py
@@ -1,1 +1,30 @@
-"This is my Seatwork" 
+def convert(usd):
+    indianRuppe = usd * 82.74
+    british = usd * 0.73
+    china = usd * 6.45
+    
+    return indianRuppe, british, china 
+
+
+
+
+def main():
+    while True:
+        dollars = input("Enter dollar ($) (*to exit): ")
+    
+        if dollars == "*":
+            print("Bye")
+            break
+        
+        parts = dollars.split("@")
+        dollars = []
+        for x in parts:
+            x = x.strip()
+            if x.replace(".", "", 1).isdigit():
+                dollars.append(float(x))
+        print("\nDollar ($)\tIndian Rupee (R)\tBritish (Pound)\tChina (Y)")
+        for usd in dollars:
+            indianRuppe, british, china = convert(usd)
+            print(f"{usd:.2f}\t\t{indianRuppe:.2f}\t\t\t{british:.2f}\t\t\t{china:.2f}")
+        print()
+main()
